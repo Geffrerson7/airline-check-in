e25da313bf7e621819d0e6dd0a259e029208ae47
Gefferson Casasola <gcasasolah@uni.pe>
2023-03-29 10:06:20 -0500

    Add redirection of the root route to the API endpoint

--- checkin/urls.py
+++ checkin/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import ApiRootView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('flights/', include("flight.urls")),
+    path('', ApiRootView.as_view(), name='api-root'),
 ]
