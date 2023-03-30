970d8e65436e9e362827c1357e2fbe3a3af0f95b
Gefferson Casasola <gcasasolah@uni.pe>
2023-03-29 23:38:08 -0500

    Add deployment settings

--- checkin/settings.py
+++ checkin/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "airline-check-in-production.up.railway.app"]
+
+CSRF_TRUSTED_ORIGINS = ["https://airline-check-in-production.up.railway.app"]
 
 
 # Application definition
